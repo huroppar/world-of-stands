760e77ef1e0ea7a03d0ef679bb098fdc3d549da8
huroppar <70945063+huroppar@users.noreply.github.com>
2025-04-14 00:56:04 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,1 @@
-print("👋 Hello from working script.lua with OrionLib!")
-
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/1iseeyou/OrionLib/main/source.lua"))()
-
-OrionLib:MakeNotification({
-    Name = "成功！",
-    Content = "OrionLib 読み込めたよ！",
-    Image = "rbxassetid://4483345998",
-    Time = 5
-})
+local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))()
