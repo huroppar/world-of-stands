c3231fb8c34c3fc3075be674999ebf0af761815b
huroppar <70945063+huroppar@users.noreply.github.com>
2025-04-14 00:47:53 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,14 @@
-print("✅ Hello from script.lua! ✅")
+print("✅ Hello from script.lua!")
+
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+print("✅ OrionLib Loaded!")
+
+local Window = OrionLib:MakeWindow({
+    Name = "🌀 Stand Power Controller",
+    HidePremium = false,
+    SaveConfig = true,
+    IntroText = "World of Stands Hack Panel",
+    ConfigFolder = "WOS_Util"
+})
+
+print("✅ Window created!")
