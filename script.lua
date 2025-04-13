1a963bbabfe368d8150541cd5653045786266aa3
huroppar <70945063+huroppar@users.noreply.github.com>
2025-04-14 00:58:16 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Theking-of-exploits-Christian-exploiter/OrionCrazyChristian/refs/heads/main/source')))()
