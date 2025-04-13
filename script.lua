cda998d12f0de13c92e67b6203a57b4deed5d4af
huroppar <70945063+huroppar@users.noreply.github.com>
2025-04-14 00:52:57 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,28 +1,10 @@
+print("👋 Hello from working script.lua with OrionLib!")
+
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/1iseeyou/OrionLib/main/source.lua"))()
 
--- ウィンドウ作成
-local Window = OrionLib:MakeWindow({
-    Name = "🛠️ Stand Power Controller",
-    HidePremium = false,
-    SaveConfig = true,
-    IntroText = "World of Stands Hack Panel",
-    ConfigFolder = "WOS_Util"
-})
-
--- セクションと通知のテスト
 OrionLib:MakeNotification({
     Name = "成功！",
-    Content = "OrionLib 読み込み成功しました！",
+    Content = "OrionLib 読み込めたよ！",
     Image = "rbxassetid://4483345998",
     Time = 5
 })
-
--- タブ作成
-local Tab = Window:MakeTab({
-    Name = "メイン",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-
--- ラベル追加
-Tab:AddLabel("準備完了！ここから機能追加していこう💪")
