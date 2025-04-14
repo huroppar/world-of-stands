af1717dab44b6c10732cac36a04e8a7d0054f441
huroppar <70945063+huroppar@users.noreply.github.com>
2025-04-15 02:20:04 +0900

    Update main script2.lua

--- main script2.lua
+++ main script2.lua
@@ -1,5 +1,4 @@
--- OrionLibの読み込み
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/WRUyYTdY"))()
 
 -- メインウィンドウ作成
 local Window = OrionLib:MakeWindow({
