b5753ed10ecdda5f70666c3eecd99912ff9684eb
huroppar <70945063+huroppar@users.noreply.github.com>
2025-04-24 12:22:57 +0900

    Update script.lua

--- script.lua
+++ script.lua
@@ -342,14 +342,6 @@ local function updatePlayerHighlights()
     end
 end
 
-VisualTab:Toggle("Character Highlight", function(state)
-getgenv().enabled = state --Toggle on/off
-getgenv().filluseteamcolor = true --Toggle fill color using player team color on/off
-getgenv().outlineuseteamcolor = true --Toggle outline color using player team color on/off
-getgenv().fillcolor = Color3.new(0, 0, 0) --Change fill color, no need to edit if using team color
-getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
-getgenv().filltrans = 0.5 --Change fill transparency
-getgenv().outlinetrans = 0.5 --Change outline transparency
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
