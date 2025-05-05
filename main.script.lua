02d9ce92897964a2cdf891f36cc1f552a3f06bdf
huroppar <70945063+huroppar@users.noreply.github.com>
2025-05-05 11:44:33 +0900

    Update main.script.lua

--- main.script.lua
+++ main.script.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 -- OrionLib読み込み
 local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/WRUyYTdY"))()
-local Window = OrionLib:MakeWindow({Name = "おっぱい", HidePremium = false, SaveConfig = true, ConfigFolder = "WOS_Config"})
+local Window = OrionLib:MakeWindow({Name = "wos script", HidePremium = false, SaveConfig = true, ConfigFolder = "WOS_Config"})
 local MainTab = Window:MakeTab({ Name = "メイン", Icon = "rbxassetid://4483345998", PremiumOnly = false })
 
 -- ScreenGuiを作成
